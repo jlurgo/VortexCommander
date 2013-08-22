485cf54cab08e7bcec1d4e19d05a1516e03f0bf8
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-22 05:39:40 -0300

    freciencia de polling http mas lenta, seteada a un segundo

--- VortexCommander.js
+++ VortexCommander.js
@@ -1,5 +1,5 @@
 $(function () {            
-    var clienteHTTP = new NodoClienteHTTP('http://router-vortex.herokuapp.com', 100);             
+    var clienteHTTP = new NodoClienteHTTP('http://router-vortex.herokuapp.com', 1000);             
     NodoRouter.instancia.conectarBidireccionalmenteCon(clienteHTTP);
     
     var nodo_app_vcommander = new AppVortexCommander();        
